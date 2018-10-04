ca04b751e8839e65b3bd9cffefa4ea3c54621169
Tona Morales-Calkins <tona.mc@gmail.com>
2018-10-04 00:36:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value) {
-   object.entries(recipes);
-   var newRecipes = recipes[key] = value;
-   object.value(recipes);
-    recipes[key] = value;
+   //object.entries(recipes);
+  // var newRecipes = recipes[key] = value;
+  // object.value(recipes);
+  //  recipes[key] = value;
     
-    return(recipes, newRecipes);
+   // return(recipes, newRecipes);
+   recipes[key]=value;
+   return(recipes);
+}
+var playlist = {artistName: `songTitle`};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return(playlist);
+}
+
+function removeFromPlaylist(playlist, artistName) {
+
+  delete playlist[artistName];
+
+  delete playlist.artistName;
+
+  return(playlist);
 }
