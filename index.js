db7ce70922dbbd928e5a5b5f19114e50e519f92b
Tona Morales-Calkins <tona.mc@gmail.com>
2018-10-04 00:21:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value) {
-<<<<<<< HEAD
    Object.entries(recipes);
    var newRecipes = recipes[key] = value;
-   return;
-=======
-  Object.value(recipes);
+   Object.value(recipes);
     recipes[key] = value;
     
-    return(recipes);
->>>>>>> 41f21b040f0b22d2a433183f6ad876504d751680
+    return(recipes, newRecipes);
+
 }
